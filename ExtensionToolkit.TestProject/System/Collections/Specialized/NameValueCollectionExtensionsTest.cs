988aa92d668a46dfecf625ab36e499caa9a21967
jbaurle_cp <SND\jbaurle_cp>
2008-01-24 10:02:59 +0000

    Added extension methods to NameValueCollection

---
+++ ExtensionToolkit.TestProject/System/Collections/Specialized/NameValueCollectionExtensionsTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Specialized;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExtensionToolkit.TestProject
+{
+	[TestClass()]
+	public class NameValueCollectionExtensionsTest
+	{
+		public TestContext TestContext { get; set; }
+
+		[TestMethod()]
+		public void ContainsKeyTest()
+		{
+			NameValueCollection c = new NameValueCollection();
+			c.Add("ABC", "ABC-val");
+			c.Add("XYZ", "XYZ-val");
+			c.Add("MIT", "MIT-val");
+			Assert.AreEqual(true, c.ContainsKey("MIT"));
+			Assert.AreEqual(true, c.ContainsKey("xyz"));
+			Assert.AreEqual(false, c.ContainsKey("BCZ"));
+		}
+	}
+}
---
+++ ExtensionToolkit/System/Collections/Specialized/NameValueCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace System.Collections.Specialized
+{
+	/// <summary>
+	/// Represents a collection of useful extenions methods.
+	/// </summary>
+	public static class NameValueCollectionExtensons
+	{
+		/// <summary>
+		/// Determines if the collection contains a specific key.
+		/// </summary>
+		public static bool ContainsKey(this NameValueCollection collection, string keyToFind)
+		{
+			if(collection == null)
+				throw new ArgumentNullException("collection");
+			if(keyToFind == null)
+				throw new ArgumentNullException("keyToFind");
+
+			string k = keyToFind.ToLower().Trim();
+
+			foreach(string key in collection)
+			{
+				if(key.ToLower().Trim().Equals(k))
+					return true;
+			}
+
+			return false;
+		}
+	}
+}
