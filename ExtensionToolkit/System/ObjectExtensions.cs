dd0265a18b845da37f69707a2670a490aa4b0fc4
Jürgen Bäurle <jbaurle@users.noreply.github.com>
2008-01-24 11:53:53 +0000

    Added extension methods to Object type (HasCustomAttribute)

---
+++ ExtensionToolkit/System/ObjectExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+
+namespace System
+{
+	/// <summary>
+	/// Represents a collection of useful extenions methods.
+	/// </summary>
+	public static class ObjectExtensions
+	{
+		/// <summary>
+		/// Indicates whether the specified attribute type is defined for the current object 
+		/// instance.
+		/// </summary>
+		public static bool HasCustomAttribute(this object o, Type attributeToFind)
+		{
+			if(typeof(Attribute).IsAssignableFrom(attributeToFind) == false)
+				throw new ArgumentException("attributeToFind");
+
+			object[] attributes = o.GetType().GetCustomAttributes(attributeToFind, true);
+
+			return (attributes != null && attributes.Length > 0);
+		}
+	}
+}
