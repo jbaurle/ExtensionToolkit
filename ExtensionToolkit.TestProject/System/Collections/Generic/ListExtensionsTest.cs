fae8977526c1d9941986097bdaf33ac76a5eb11c
jbaurle_cp <SND\jbaurle_cp>
2008-01-24 10:58:41 +0000

    Added extension methods to List<string> (ToDataTableStructure)

---
+++ ExtensionToolkit.TestProject/System/Collections/Generic/ListExtensionsTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExtensionToolkit.TestProject
+{
+	[TestClass()]
+	public class ListExtensionsTest
+	{
+		public TestContext TestContext { get; set; }
+
+		[TestMethod()]
+		public void ToDataTableStructureTest()
+		{
+			List<string> columns = new List<string>();
+			columns.Add("ID");
+			columns.Add("Name");
+			DataTable dt = columns.ToDataTableStructure();
+			Assert.AreEqual("ID", dt.Columns[0].ColumnName);
+			Assert.AreEqual("Name", dt.Columns[1].ColumnName);
+		}
+	}
+}
