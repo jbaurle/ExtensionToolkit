f13b86b7a894a0d445f622b0413964eaa26d519f
jbaurle_cp <SND\jbaurle_cp>
2008-01-24 11:53:53 +0000

    Added extension methods to Object type (HasCustomAttribute)

---
+++ ExtensionToolkit.TestProject/System/ObjectExtensionsTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExtensionToolkit.TestProject
+{
+	[TestClass()]
+	public class ObjectExtensionsTest
+	{
+		public TestContext TestContext { get; set; }
+
+		[TestMethod()]
+		public void HasCustomAttributeTest()
+		{
+			ObjectExtensionsTest tc = new ObjectExtensionsTest();
+			Assert.AreEqual(true, tc.HasCustomAttribute(typeof(TestClassAttribute)));
+			Assert.AreEqual(false, tc.HasCustomAttribute(typeof(TestMethodAttribute)));
+		}
+	}
+}
