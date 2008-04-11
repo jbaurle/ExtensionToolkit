5a25da538d5a452d03ab6884ba7721654e1b231b
doggy8088_cp <SND\doggy8088_cp>
2008-04-11 13:16:37 +0000

    Add System/Web/Caching/CacheExtension.cs for extend Clear() method for System.Web.Caching.Cache class.

---
+++ ExtensionToolkit/System/Web/Caching/CacheExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Caching;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Summary description for System.Web.Caching
+/// </summary>
+static public class CacheExtensionMethod
+{
+    public static void Clear(this Cache x)
+    {
+        List<string> cacheKeys = new List<string>();
+
+        IDictionaryEnumerator cacheEnum = x.GetEnumerator();
+
+        while (cacheEnum.MoveNext())
+        {
+            cacheKeys.Add(cacheEnum.Key.ToString());
+        }
+
+        foreach (string cacheKey in cacheKeys)
+        {
+            x.Remove(cacheKey);
+        }
+    }
+
+}
